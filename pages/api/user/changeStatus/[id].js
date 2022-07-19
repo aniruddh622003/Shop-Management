cf940ccd4c9cf1860738440256418ea35c185066
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2022-07-20 00:41:00 +0530

    Current user check

--- pages/api/user/changeStatus/[id].js
+++ pages/api/user/changeStatus/[id].js
@@ -4,12 +4,19 @@ import { prismaClient } from "../../../../utils/prisma";
 async function handler(req, res) {
   const prisma = prismaClient;
   const { id } = req.query;
+
   if (req.method == "POST") {
     const { body } = req;
+    if (req.user.id == parseInt(id)) {
+      return res.status(403).json({
+        message: "Can not change user though which you are logged in.",
+      });
+    }
     const user = await prisma.user.update({
       where: { id: parseInt(id) },
       data: { enabled: JSON.parse(body.enabled) },
     });
+
     delete user.password;
     res.status(201).send(user);
   } else {
