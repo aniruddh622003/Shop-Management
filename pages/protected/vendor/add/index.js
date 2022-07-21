546dd4ad50c1afc4836998920b89a31f28defea0
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2022-07-22 02:51:55 +0530

    forgot what i did :)

--- pages/protected/vendor/add/index.js
+++ pages/protected/vendor/add/index.js
@@ -1,20 +1,13 @@
 import React, { useCallback } from "react";
 import { Box, Typography, Grid, Button } from "@mui/material";
 import { Formik, Form } from "formik";
-import InputField from "../../../../components/shared/InputField";
+import InputField from "components/shared/InputField";
 import { useSnackbar } from "notistack";
 import { useMutation } from "react-query";
-import VendorService from "../../../../services/Vendor";
+import VendorService from "/services/Vendor";
 import { useRouter } from "next/router";
-import * as Yup from "yup";
 
-const ValidationScheme = Yup.object().shape({
-  name: Yup.string().required("Please enter vendor name"),
-  contact: Yup.number()
-    .required("Please enter contact number")
-    .typeError("Contact number should be a number"),
-  address: Yup.string().required("Please enter vendor address"),
-});
+import VendorForm from "components/vendor/VendorForm";
 
 const AddVendorPage = () => {
   const { enqueueSnackbar } = useSnackbar();
@@ -55,34 +48,7 @@ const AddVendorPage = () => {
       <Typography variant="h5" component="div">
         Add Vendor
       </Typography>
-      <Formik
-        initialValues={{}}
-        validationSchema={ValidationScheme}
-        onSubmit={handleSubmit}
-      >
-        <Form style={{ width: "100%" }}>
-          <Grid container mt={5} columnSpacing={3}>
-            <Grid item md={6}>
-              <InputField name="name" label="Vendor Name" sx={{ mb: 1 }} />
-            </Grid>
-            <Grid item md={6}>
-              <InputField name="contact" label="Contact" sx={{ mb: 1 }} />
-            </Grid>
-            <Grid item md={12}>
-              <InputField
-                name="address"
-                label="Vendor Address"
-                sx={{ mb: 1 }}
-                multiline
-                minRows={4}
-              />
-              <Button variant="contained" color="secondary" type="submit">
-                Create Vendor
-              </Button>
-            </Grid>
-          </Grid>
-        </Form>
-      </Formik>
+      <VendorForm initVals={{}} handleSubmit={handleSubmit} />
     </Box>
   );
 };
---
+++ pages/protected/vendor/edit/[id].js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditVendorPage = () => {
+  return <div>EditVendorPage</div>;
+};
+
+export default EditVendorPage;
