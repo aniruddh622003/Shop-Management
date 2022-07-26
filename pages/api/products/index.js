fbdf28be76f96daf38260a2c40abe0d49d798b2d
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2022-07-26 06:02:25 +0530

    Purchase Product Logic Done
    Signed-off-by: Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -14,53 +14,90 @@ async function handler(req, res) {
   } else if (req.method == "POST") {
     const { body } = req;
     console.log("products");
-    console.log(body);
+    console.log(typeof body.products);
+    let purchaseIDs = [];
 
-    const checkProduct = await prisma.products.findFirst({
-      where: {
-        name: body.name,
-        MRP: body.mrp,
-        vendorID: body.vendorID,
+    if (typeof body.products[0] == "string" || !body.products[0]) {
+      return res
+        .status(400)
+        .json({ message: "Expected Products to be an array." });
+    }
+
+    await Promise.all(
+      body.products.map(async (ele, idx) => {
+        let productID;
+        const checkProduct = await prisma.products.findFirst({
+          where: {
+            name: ele.name,
+            MRP: ele.mrp,
+          },
+        });
+        if (!checkProduct) {
+          const newProduct = await prisma.products.create({
+            data: {
+              name: ele.name,
+              BuyPrice: ele.buyPrice,
+              MRP: ele.mrp,
+              Quantity: ele.quantity,
+              vendor: {
+                connect: {
+                  id: ele.vendorID,
+                },
+              },
+            },
+          });
+          productID = newProduct.id;
+        } else {
+          let newQty = ele.quantity + checkProduct.Quantity;
+          let newBuy =
+            (ele.quantity * ele.buyPrice +
+              checkProduct.Quantity * checkProduct.BuyPrice) /
+            newQty;
+
+          const newProduct = await prisma.products.update({
+            where: {
+              name_MRP: {
+                name: ele.name,
+                MRP: ele.mrp,
+              },
+            },
+            data: {
+              name: ele.name,
+              BuyPrice: newBuy,
+              MRP: ele.mrp,
+              Quantity: newQty,
+              vendor: {
+                connect: {
+                  id: ele.vendorID,
+                },
+              },
+            },
+          });
+          productID = newProduct.id;
+        }
+        console.log(productID);
+        const newPurchaseItem = await prisma.purchaseItem.create({
+          data: {
+            productID: productID,
+            vendorID: ele.vendorID,
+            Quantity: ele.quantity,
+            PurchasePrice: ele.buyPrice,
+          },
+        });
+        purchaseIDs.push({ id: newPurchaseItem.id });
+      })
+    );
+    console.log(purchaseIDs);
+    const newPurchase = await prisma.purchase.create({
+      data: {
+        purchaseItems: {
+          connect: [...purchaseIDs],
+        },
+        purchasedByID: req.user.id,
+        Note: body.note,
       },
     });
-
-    if (!checkProduct) {
-      const newProduct = await prisma.products.create({
-        data: {
-          name: body.name,
-          BuyPrice: body.buyPrice,
-          MRP: body.mrp,
-          Quantity: body.quantity,
-          vendorID: body.vendorID,
-        },
-      });
-      return res.status(201).send(newProduct);
-    } else {
-      let newQty = body.quantity + checkProduct.Quantity;
-      let newBuy =
-        (body.quantity * body.buyPrice +
-          checkProduct.Quantity * checkProduct.BuyPrice) /
-        newQty;
-
-      const newProduct = await prisma.products.update({
-        where: {
-          name_MRP_vendorID: {
-            name: body.name,
-            MRP: body.mrp,
-            vendorID: body.vendorID,
-          },
-        },
-        data: {
-          name: body.name,
-          BuyPrice: newBuy,
-          MRP: body.mrp,
-          Quantity: newQty,
-          vendorID: body.vendorID,
-        },
-      });
-
-      return res.status(200).send(newProduct);
-    }
+    return res.status(200).json({ message: "Products added successfully" });
   } else {
     res.status(405).json({ message: "Wrong HTTP Method" });
   }
